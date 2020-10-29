9ca24e6aed5199558905cfc58f3e5b1c0065d800
Assis <assishenrique@gmail.com>
2020-10-29 16:47:24 -0300

    Config files

---
+++ configLinux.sh
@@ -0,0 +1,8 @@
+sudo python3 -m pip install virtualenv
+
+virtualenv env
+
+source env/bin/activate
+
+python3 -m pip install flask
+python3 -m pip install pandas
---
+++ configWindows.bat
@@ -0,0 +1,8 @@
+python3 -m pip install virtualenv
+
+virtualenv env
+
+source env/bin/activate
+
+python3 -m pip install flask
+python3 -m pip install pandas
